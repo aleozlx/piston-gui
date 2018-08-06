5437faf0b2b757e0d38360ddcdfa655039e081c7
Alex Yang <aleozlx@gmail.com>
2018-08-05 23:14:22 -0500

    new sample data

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,7 @@ fn main() {
     let mut scene = Scene::new();
     let font = vgui::load_font("FiraSans-Regular.ttf").expect("Cannot load font.");
 
-    let assets = find_folder::Search::ParentsThenKids(2, 2).for_folder("assets").unwrap();
-    let ref fname_h5meta = assets.join("epoch1.h5.txt");
+    let ref fname_h5meta = std::path::PathBuf::from("/home/alex/datasets/ucm-sample.h5.txt");
     let mut menu;
     match h5ls_reader::parse(fname_h5meta) {
         Ok(root) => {
