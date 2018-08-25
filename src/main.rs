522e17988ba486a858e5e9f05c1d3b58c6629fd1
Alex Yang <aleozlx@gmail.com>
2018-08-24 23:20:15 -0500

    ..

--- src/main.rs
+++ src/main.rs
@@ -134,7 +134,22 @@ fn main() {
                                             println!("shape {:?}", &d.shape);
                                         }
                                         
-                                        // TODO make a sample query
+                                        // TODO request different images
+                                        if let Some(im) = image_cache.request_one(&H5URI{
+                                            path: String::from("/home/alex/datasets/ucm-sample.h5"),
+                                            h5path: String::from("/source/images"),
+                                            query: String::from("13"),
+                                            dtype: Dtype::F4
+                                        }, &(224,224)) {
+                                            let tex = Rc::new(Texture::from_image(
+                                                &mut window.factory,
+                                                &im,
+                                                &TextureSettings::new()
+                                            ).unwrap());
+                                            let mut sprite_tex = Sprite::from_texture(tex.clone());
+                                            sprite_tex.set_position(120.0, 320.0);
+                                            let _id_tex = scene.add_child(sprite_tex);
+                                        }
                                         
                                         h5pointer.pop();
                                     }
