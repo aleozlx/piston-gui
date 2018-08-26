876e80568a02bb1bb6609b5b7d707ff22c329962
Alex Yang <aleozlx@gmail.com>
2018-08-25 21:23:03 -0500

    refactor for prefetch

--- src/h5slice.rs
+++ src/h5slice.rs
@@ -7,6 +7,7 @@ use std::net::TcpStream;
 use flate2::read::GzDecoder;
 use std::string::ToString;
 use std::collections::HashMap;
+use std::ops::Deref;
 
 pub type TexImage = image::RgbaImage;
 
@@ -80,26 +81,73 @@ impl H5Cache {
         }
     }
 
-    fn fetch_one(&mut self, uri: &H5URI, resolution: (u32, u32)) -> Option<&'_ mut TexImage> {
-        fn __fetch_one(uri: &H5URI, resolution: (u32, u32)) -> Option<TexImage> {
-            let mut stream = TcpStream::connect("localhost:8000").ok()?;
-            let mut buffer_in = Vec::with_capacity(8<<10);
-            let mut buffer_out = Vec::with_capacity(4<<20);
-            let _ = stream.write(uri.to_string().as_bytes());
-            let n = stream.read_to_end(&mut buffer_in).ok()?;
-            // TODO use logging instead
-            if cfg!(debug_assertions) { println!("Read {} bytes from network.", n); }
-            let mut decoder = GzDecoder::new(&buffer_in[..]);
-            let n = decoder.read_to_end(&mut buffer_out).ok()?;
-            if cfg!(debug_assertions) { println!("Decompressed into {} bytes.", n); }
-            let im_rgb = image::ImageBuffer::from_raw(resolution.0, resolution.1,
-                unsafe { slice::from_raw_parts(buffer_out.as_ptr() as *const f32, n/mem::size_of::<f32>()) }
-                    .into_iter().map(|x| {(x+100.0) as u8}).collect())?;
-            // A copy is done by format conversion, so lifetime is correct.
-            Some(image::DynamicImage::ImageRgb8(im_rgb).to_rgba())
+    fn download(uri: &H5URI) -> Option<Vec<u8>> {
+        let mut stream = TcpStream::connect("localhost:8000").ok()?;
+        let mut buffer_in = Vec::with_capacity(8<<10);
+        let mut buffer_out = Vec::with_capacity(4<<20);
+        let _ = stream.write(uri.to_string().as_bytes());
+        let n = stream.read_to_end(&mut buffer_in).ok()?;
+        // TODO use logging instead
+        if cfg!(debug_assertions) { println!("Read {} bytes from network.", n); }
+        let mut decoder = GzDecoder::new(&buffer_in[..]);
+        let n = decoder.read_to_end(&mut buffer_out).ok()?;
+        if cfg!(debug_assertions) { println!("Decompressed into {} bytes.", n); }
+        return Some(buffer_out);
+    }
+
+    fn deserialize(buffer: &Vec<u8>, resolution: &(u32, u32), offset: isize) -> Option<TexImage> {
+        let data = unsafe {
+            slice::from_raw_parts(
+                (buffer.as_ptr() as *const f32).offset(offset),
+                mem::size_of::<f32>()*((resolution.0*resolution.1*3) as usize))
+        };
+
+        Some(image::DynamicImage::ImageRgb8(image::ImageBuffer::from_raw(
+            resolution.0, resolution.1,
+            data.into_iter().map(|x| {
+                (x+100.0) as u8
+            }
+        ).collect())?).to_rgba())
+    }
+
+    pub fn prefetch(&mut self, uri: &H5URI, resolution: (u32, u32)) {
+        if let Some(buffer_out) = H5Cache::download(uri) {
+            match uri.query {
+                Query::One(_) => {
+                    if let Some(im_rgba) = H5Cache::deserialize(&buffer_out, &resolution, 0) {
+                        self.buffer.insert(uri.clone(), im_rgba);
+                    }
+                },
+                Query::Batch(idx, len) => {
+
+                },
+                Query::Range(a, b) => {
+
+                }
+            }
         }
-        
-        self.buffer.insert(uri.clone(), __fetch_one(uri, resolution)?);
-        Some(self.buffer.get_mut(uri).unwrap())
+    }
+
+    fn auto_prefetch_uri(&self, uri_original: &H5URI) -> H5URI {
+        let mut uri = uri_original.clone();
+        // match uri.query {
+        //     Query::One(idx) => {
+        //         // ? check overlap?
+        //         uri.query = Query::Batch(idx, self.hint);
+        //     },
+        //     _ => {}
+        // }
+        return uri;
+    }
+
+    fn fetch_one(&mut self, uri: &H5URI, resolution: (u32, u32)) -> Option<&'_ mut TexImage> {
+        match uri.query {
+            Query::One(_) => {
+                let uri_prefetch = self.auto_prefetch_uri(uri);
+                self.prefetch(&uri_prefetch, resolution);
+                Some(self.buffer.get_mut(uri).unwrap())
+            },
+            _ => None // uri is ensured to be One because this function is private!
+        }
     }
 }
